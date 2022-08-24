7489d0cf514a7c951234eb7157cf1a181f0b0edc
rhaun12 <62476343+rhaun12@users.noreply.github.com>
2022-08-23 20:15:59 -0400

    Create teleport-to-new.lua

---
+++ teleport-to-new.lua
@@ -0,0 +1,10 @@
+local Players = game:GetService("Players")
+local ServerScriptService = game:GetService("ServerScriptService")
+ 
+local SafeTeleport = require(ServerScriptService.SafeTeleport)
+ 
+local TARGET_PLACE_ID = 155615604 -- replace with your own place ID
+ 
+local playerToTeleport = Players:GetPlayers()[1] -- get the first player in the game
+ 
+SafeTeleport(TARGET_PLACE_ID, {playerToTeleport})
